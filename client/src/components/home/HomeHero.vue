91477c66943c249b33dce3ed62595018972e582d
João <joao@madebyjoao.fr>
2026-07-13 10:16:10 +0200

    feat: implementation of a Hero view

---
+++ client/src/components/home/HomeHero.vue
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<template>
+
+    <section class="min-h-dvh flex flex-col justify-center px-6 ">
+        
+        <div class="">
+            test
+        </div>
+        <div class="">
+            test
+        </div>
+        <div class="">
+            test
+        </div>
+        <div class="">
+            test
+        </div>
+
+    </section>
+
+</template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
+import HomeHero from '@/components/home/HomeHero.vue';
+
 
 </script>
 
 <template>
 	<!-- no <main> wrapper: the layout owns <main> -->
-		
+	<HomeHero />
 </template>
